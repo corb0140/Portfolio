02b13bc089515ec829f687da3af0ef3693464e9f
corb0140 <corb0140@algonquinlive.com>
2026-07-25 12:29:42 -0400

    updated ui for welcom page

--- app/pages/WelcomePage.tsx
+++ app/pages/WelcomePage.tsx
@@ -17,14 +17,32 @@ export default function Welcome() {
   );
 
   return (
-    <div
-      className="relative h-screen w-screen flex items-center justify-center 
-    bg-linear-to-br from-bg-secondary from-30% to-bg to-70%"
-    >
-      <div className="hidden desktop:block h-full w-full border-x border-white mx-25" />
-      <div className="hidden desktop:block absolute top-15 bottom-0 left-0 h-[85%] w-full border-y border-white" />
+    <div className="relative h-dvh w-screen flex items-center justify-center overflow-hidden bg-bg-secondary">
+      {/* GRID PATTERN */}
+      <svg className="absolute inset-0 w-full h-full">
+        <defs>
+          <pattern
+            id="grid"
+            width="40"
+            height="40"
+            patternUnits="userSpaceOnUse"
+          >
+            <path
+              d="M 40 0 L 0 0 0 40"
+              fill="none"
+              stroke="rgba(255,255,255,0.03)"
+              strokeWidth="1"
+            />
+          </pattern>
+        </defs>
+        <rect width="100%" height="100%" fill="url(#grid)" />
+      </svg>
 
-      <span className="text-[14px] ipad:text-5xl desktop:text-7xl font-bold uppercase tracking-[15px] ipad:tracking-[20px] text-white absolute flex">
+      {/* TINT OVERLAY */}
+      <div className="absolute inset-0 bg-linear-to-br from-bg/10 via-bg-tertiary/15 to-bg-secondary/30" />
+
+      {/* TITLE */}
+      <span className="relative z-10 text-[14px] ipad:text-5xl desktop:text-7xl font-bold uppercase tracking-[15px] ipad:tracking-[20px] text-white flex">
         {letters.map((char, i) => (
           <AnimatedLetter
             key={i}
@@ -35,6 +53,7 @@ export default function Welcome() {
         ))}
       </span>
 
+      {/* BUTTON */}
       <motion.button
         initial={{ opacity: 0, y: 10 }}
         animate={{ opacity: 1, y: 0 }}
@@ -43,7 +62,7 @@ export default function Welcome() {
           scale: [1, 1.1, 1],
           transition: { duration: 1, ease: "easeInOut", repeat: Infinity },
         }}
-        className="absolute bottom-20 right-30 flex items-center justify-center text-white text-[12px] ipad:text-[16px] desktop:text-[12px] tracking-wider uppercase"
+        className="absolute z-10 bottom-20 right-30 flex items-center justify-center text-white text-[12px] ipad:text-[16px] desktop:text-[12px] tracking-wider uppercase"
         onClick={() => navigate("/home")}
       >
         View Portfolio
